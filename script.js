1b3291890bdf03b19ff7d78652a7b80d13d5a575
apprentice　programer <97390204+web-apprentice@users.noreply.github.com>
2022-02-27 13:12:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-$(function(){
-('#page-link a[href*="#"]').click(function () {
-    var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-    var pos = $(elmHash).offset().top;  //idの上部の距離を取得
-    $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-    return false;
+
+$(function() {
+    var $nav   = $('#navArea');
+    var $btn   = $('.toggle_btn');
+    var $mask  = $('#mask');
+    var open   = 'open'; // class
+    // menu open close
+    $btn.on( 'click', function() {
+      if ( ! $nav.hasClass( open ) ) {
+        $nav.addClass( open );
+      } else {
+        $nav.removeClass( open );
+      }
+    });
+    // mask close
+    $mask.on('click', function() {
+      $nav.removeClass( open );
+    });
+  } )(jQuery);
+  
+  
+  $(function(){
+    $('header h1').click(function() {
+        var $answer = $('.mainVidual');
+        if($answer.hasClass('open1')) { 
+          $answer.removeClass('open1');
+          // slideUpメソッドを用いて、$answerを隠してください
+          $answer.fadeOut(1000);
+        } else {
+          $answer.addClass('open1'); 
+          // slideDownメソッドを用いて、$answerを表示してください
+          $answer.fadeIn(1000);
+        }
+      });
   });
-});
+
+
+
